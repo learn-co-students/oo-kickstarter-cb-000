000c662c6b01d948039709007651fff3893b0141
Mark Fowler <fowlermark488@gmail.com>
2017-11-24 06:12:36 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,6 +1,6 @@
 class Backer
 
-  attr_accessor :backed_projects, :name
+  attr_accessor :name, :backed_projects
 
   def initialize(name)
     @name = name
@@ -11,6 +11,5 @@ class Backer
     @backed_projects << project
     project.backers << self
   end
-
 
 end
