96a35dca470b4388e8d163cd7204e0f5df76a1e3
Jen Poole <contact.vandepool@gmail.com>
2017-12-17 21:38:54 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,7 +9,9 @@ class Backer
   end
 
   # accepts a Project as an argument and stores it in a backed_projects array
+  # also adds the backer to the project's backers array
   def back_project(project)
     backed_projects << project
+    project.backers << self
   end
 end
