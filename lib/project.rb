2af11d1bb738a06d709b628a810790e49cd7f0cd
Mitesh Patel <mit06@hotmail.com>
2018-09-03 11:34:47 +0000

    Done.

--- lib/project.rb
+++ lib/project.rb
@@ -1,3 +1,21 @@
+require 'pry'
 class Project
-
+  attr_accessor :title, :backers
+  @@all = []
+  def initialize(title)
+    @title = title
+    @backers = []
+    @@all << self
+  end
+  def add_backer(backer)
+    @backers << backer
+    @backers.map {|backer| backer.backed_projects << self}
+  end
+  def self.find_or_create(project)
+    if @@all.include?(project)
+      project
+    else
+      Project.new(project)
+    end
+  end
 end
